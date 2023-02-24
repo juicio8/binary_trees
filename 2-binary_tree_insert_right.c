7ff96ea5ab04c1205e9c8f96cb9299b51a0d2734
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-02-24 10:08:06 +0100

    initial commit

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,35 +15,21 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+    binary_tree_t *node;
+
     if (parent == NULL)
-        return NULL;
+        return (NULL);
 
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-        return NULL;
+    node = binary_tree_node(parent, value);
+    if (node == NULL)
+        return (NULL);
 
-    binary_tree_t *binary_tree_insert_left(binary_tree_t * parent, int value)
+    if (parent->right != NULL)
     {
-        binary_tree_t *node;
-
-        if (parent == NULL)
-            return (NULL);
-
-        node = binary_tree_node(parent, value);
-        if (node == NULL)
-            return (NULL);
-
-        if (parent->right != NULL)
-        {
-            node->right = parent->right;
-            parent->right->parent = node;
-        }
-
-        parent->right = node;
-        return (node);
+        node->right = parent->right;
+        parent->right->parent = node;
     }
 
-    parent->right = new_node;
-    return new_node;
+    parent->right = node;
+    return (node);
 }
